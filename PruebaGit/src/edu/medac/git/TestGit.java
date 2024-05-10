9b56b920674c3ce3030237824b49ee44a1f441a6
bc <bcj0002@alu.medac.es>
2024-05-10 09:01:47 +0200

    Recuperamos la Primera versión de la clase TestGit.java

--- PruebaGit/src/edu/medac/git/TestGit.java
+++ PruebaGit/src/edu/medac/git/TestGit.java
@@ -4,7 +4,6 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		System.out.println("*** Imprimiendo desde TestGit");
-		System.out.println("*** Imprimiendo un nuevo mensaje desde TestGit");
 
 	}
 
