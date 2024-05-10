671128d27b59cdeeae8382c1a83a15aa3b9cd75e
bc <bcj0002@alu.medac.es>
2024-05-10 08:48:07 +0200

    Se agrega un nuevo println al TestGit.java

--- PruebaGit/src/edu/medac/git/TestGit.java
+++ PruebaGit/src/edu/medac/git/TestGit.java
@@ -4,6 +4,7 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		System.out.println("*** Imprimiendo desde TestGit");
+		System.out.println("*** Imprimiendo un nuevo mensaje desde TestGit");
 
 	}
 
