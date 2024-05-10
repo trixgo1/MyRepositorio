ea707e7a00b3cb54a0e40b3f55de70149a8d2d45
bc <bcj0002@alu.medac.es>
2024-05-10 08:38:28 +0200

    Primera Versión del proyectoErr

---
+++ PruebaGit/src/edu/medac/git/TestGit.java
@@ -0,0 +1,10 @@
+package edu.medac.git;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		System.out.println("*** Imprimiendo desde TestGit");
+
+	}
+
+}
---
+++ PruebaGit/src/edu/medac/git/TestGitDos.java
@@ -0,0 +1,9 @@
+package edu.medac.git;
+
+public class TestGitDos {
+
+	public static void main(String[] args) {
+		System.out.println("*** Imprimiendo desde TestGitDos");
+	}
+
+}
